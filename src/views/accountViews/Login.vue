4b2525b1cdc14f90b4cd43435c0b3847212dacb1
Kukulim <beckon@interia.pl>
2021-02-24 13:38:01 +0100

    login validations

--- src/views/accountViews/Login.vue
+++ src/views/accountViews/Login.vue
@@ -4,25 +4,60 @@
     <div class="row container">
       <div class="col">
         <form @submit="Login($event)">
-          <div class="form-group input-wrapper">
-            <label for="exampleInputEmail1">Email</label>
+         <div class="form-group input-wrapper">
+            <label>Email:</label>
             <i class="fa fa-envelope input-icon"></i>
             <input
-              type="email"
               class="form-control"
-              placeholder="email@email..."
-              v-model="currentUser.email"
+              placeholder="email..."
+              v-model.trim="$v.currentUser.email.$model"
+              :class="{
+                'is-invalid': $v.currentUser.email.$error,
+                'is-valid': !$v.currentUser.email.$invalid
+              }"
             />
+            <div class="valid-feedback">Email is ok.</div>
+            <div class="invalid-feedback">
+              <span v-if="!$v.currentUser.email.required"
+                >Email is required.</span
+              >
+              <span v-if="!$v.currentUser.email.minLength"
+                >Email must be a valid email address.</span
+              >
+              <span />
+            </div>
           </div>
           <div class="form-group input-wrapper">
-            <label for="exampleInputPassword1">Password</label>
+            <label>Password</label>
             <i class="fa fa-key input-icon"></i>
             <input
-              type="password"
               class="form-control"
-              placeholder="password..."
-              v-model="currentUser.password"
+              type="password"
+              placeholder="email..."
+              v-model.trim="$v.currentUser.password.$model"
+              :class="{
+                'is-invalid': $v.currentUser.password.$error,
+                'is-valid': !$v.currentUser.password.$invalid
+              }"
             />
+            <div class="valid-feedback">Password is ok.</div>
+            <div class="invalid-feedback">
+              <span v-if="!$v.currentUser.password.required"
+                >Password is required.</span
+              >
+              <span v-if="!$v.currentUser.password.minLength"
+                >Password must be atleast
+                {{ $v.currentUser.password.$params.minLength.min }} charakters
+                <br
+              /></span>
+              <span v-if="!$v.currentUser.password.alpha"
+                >Password must contain at least 1 numeric character <br
+              /></span>
+              <span v-if="!$v.currentUser.password.beta"
+                >Password must contain at least 1 uppercase character</span
+              >
+              <span />
+            </div>
           </div>
           <div>
             <router-link to="/forgottenpassword">
@@ -57,6 +92,18 @@
 
 <script>
 import { mapActions } from "vuex";
+import {
+  required,
+  minLength,
+  email,
+  helpers
+} from "vuelidate/lib/validators";
+
+var numberRegex = new RegExp("(?=.*[0-9])");
+var UpperRegex = new RegExp("(?=.*[A-Z])");
+
+const alpha = helpers.regex("alpha", numberRegex);
+const beta = helpers.regex("alpha", UpperRegex);
 
 export default {
   data() {
@@ -74,6 +121,11 @@ export default {
     async Login(event) {
       event.preventDefault();
 
+      this.$v.$touch();
+      if (this.$v.$invalid) {
+        return;
+      }
+
       const response = await this.loginAction(this.currentUser);
       if (response == "error") {
         this.showerrormesage = true;
@@ -82,6 +134,20 @@ export default {
         await this.getNotificationAction(response.accessToken);
         this.$router.push({ name: "NotificacionsAllCustomers" });
       } else this.$router.push({ name: "NotificationWelcome" });
+    }
+  },
+  validations: {
+    currentUser: {
+      password: {
+        required,
+        minLength: minLength(8),
+        alpha,
+        beta
+      },
+      email: {
+        required,
+        email
+      }
     }
   }
 };
